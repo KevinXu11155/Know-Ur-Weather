0ae8398c0b1787f9f8806e79ddbfdb66656ddf05
Aquila <aquila.zhou1@gmail.com>
2025-01-18 22:10:04 +0800

    整合会议记录

--- entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/oh_modules/.ohpm/@ohos+pinyin4js@2.0.0/oh_modules/@ohos/pinyin4js/index.ts
+++
@@ -1,3 +0,0 @@
-export { pinyin4js } from "@normalized:N&&&@ohos/pinyin4js/src/main/ets/components/index&2.0.0";
-export { PinyinHelper, PinyinFormat } from "@normalized:N&&&@ohos/pinyin4js/src/main/ets/components/src/PinyinHelper&2.0.0";
-export { ChineseHelper } from "@normalized:N&&&@ohos/pinyin4js/src/main/ets/components/src/ChineseHelper&2.0.0";
